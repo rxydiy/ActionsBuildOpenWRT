0c135011e167bdf2e988e79434053d581199b311
rxydiy <31360899+rxydiy@users.noreply.github.com>
2026-07-09 21:37:19 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
-
 FEEDS_CONF=feeds.conf.default
+
+# 替换高版本golang feeds包（解决mosdns/mympd编译失败）
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
+
+# 替换完feeds文件立刻更新安装，不会被外部命令覆盖
 ./scripts/feeds update -a
 ./scripts/feeds install -a
 
-# 清理旧feeds源
+# 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
 rm -rf feeds/luci/applications/luci-app-passwall
@@ -17,11 +22,7 @@ rm -rf feeds/luci/applications/luci-app-openlist
 rm -rf feeds/packages/net/openlist
 rm -rf feeds/packages/net/mosdns
 
-# 替换高版本golang feeds包（核心）
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-
-# 拉取第三方插件
+# 拉取第三方插件示例
 git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky   #  lucky 
 git clone https://github.com/sirpdboy/luci-app-taskplan.git package/luci-app-taskplan               #  定时任务设置
 
@@ -35,11 +36,3 @@ git clone --depth 1 https://github.com/jcorporation/myMPD.git /tmp/tmp_mympd
 mv /tmp/tmp_mympd/contrib/packaging/openwrt package/mympd/
 rm -rf /tmp/tmp_mympd
 sed -i '78s/^[[:space:]]*/\t/' package/mympd/Makefile
-
-
-
-# 锁定兼容mosdns，双重保险
-rm -rf package/small/mosdns
-git clone -b v5.3.1 https://github.com/sbwml/luci-app-mosdns package/small/mosdns
-
-echo "diy-part1 执行完成"
