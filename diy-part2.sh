217d723a11bb0fc6b3c30b6f89cdebeeb4258901
rxydiy <31360899+rxydiy@users.noreply.github.com>
2026-07-09 15:12:11 +0800

    更新 diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -6,10 +6,6 @@ set -e
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-sudo apt install libfuse-dev 
-rm -rf feeds/packages/lang/golang
-git clone https://github.com/sbwml/packages_lang_golang -b 26.x feeds/packages/lang/golang
-
 
 
 echo "✅ diy-part2 路由默认参数修改完成"
